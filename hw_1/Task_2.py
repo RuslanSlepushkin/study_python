49feffd93d3a4bb38d28280f82274ae331d10d7d
Ruslan Slepushkin <slepushkinruslan@gmail.com>
2023-03-16 10:20:41 +0200

    add hw_1

---
+++ hw_1/Task_2.py
@@ -0,0 +1,11 @@
+# I am studying Python!
+print('I ' + 'am ' + 'studying ' + 'Python', end = '!')
+print('\nI', 'am', 'studying', 'Python', sep = " ", end = '!\n')
+
+
+
+# My name is Ruslan! I'm 29 years old.
+name = 'Ruslan!'
+old = 29
+print("My name is", name, "I'm", old, 'years old', end = '.')
+print(f'\nMy name is {name} I\'m {old} years old.')
